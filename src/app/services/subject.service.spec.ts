a16a184f2fb4f1627677f85a1de7dd75d43057f1
Arlen Neylon <arlen.m.neylon@gmail.com>
2021-05-03 15:18:11 -0600

    Start setting up and fixing tests.

--- src/app/services/subject.service.spec.ts
+++ src/app/services/subject.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SubjectService } from './subject.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('SubjectService', () => {
   let service: SubjectService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SubjectService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        SubjectService
+      ]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController)
+    service = TestBed.inject(SubjectService)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all subjects', () => {
+    service.getSubjects().subscribe()
+    const req = httpTestingController.expectOne('http://localhost:3000/subject')
+    req.flush([
+      { id: 1, title: 'One', subTitle: '1' }
+    ])
+    httpTestingController.verify()
+  })
 });
